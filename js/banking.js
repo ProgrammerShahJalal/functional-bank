cb0ac1743fa56da7d04fa22ee7300484f5f64502
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-08-17 19:58:00 +0600

    banking completed

--- js/banking.js
+++ js/banking.js
@@ -1,1 +1,41 @@
-console.log('banking js added')
+document.getElementById('deposite-button').addEventListener('click', function () {
+    const depositeInput = document.getElementById('deposite-input');
+    const depositeAmountText = depositeInput.value;
+    const depositeAmount = parseFloat(depositeAmountText);
+
+    // get current deposite
+    const depositeTotal = document.getElementById('deposite-total');
+    const depositeText = depositeTotal.innerText;
+    const previousdepositeTotalAmount = parseFloat(depositeText);
+    depositeTotal.innerText = previousdepositeTotalAmount + depositeAmount;
+
+    // update balance total
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceText = balanceTotal.innerText;
+    const previousBalanceTotalAmount = parseFloat(balanceText);
+    balanceTotal.innerText = previousBalanceTotalAmount + depositeAmount;
+
+    // clear the deposite input field
+    depositeInput.value = '';
+});
+// handle withdraw button
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawInputText = withdrawInput.value;
+    const withdrawInputTotal = parseFloat(withdrawInputText);
+
+    // update withdraw total
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawText = withdrawTotal.innerText;
+    const withdrawTotalAmount = parseFloat(withdrawText);
+    withdrawTotal.innerText = withdrawTotalAmount + withdrawInputTotal;
+
+    // update balance total after withdraw
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceText = balanceTotal.innerText;
+    const previousBalanceTotalAmount = parseFloat(balanceText);
+    balanceTotal.innerText = previousBalanceTotalAmount - withdrawInputTotal;
+
+    // clear the withdraw input field
+    withdrawInput.value = '';
+});
